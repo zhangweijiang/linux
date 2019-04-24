e80e34e7102c17269d927b950b2f0c6c792cb721
张伟江 <982215226@qq.com>
2019-04-24 15:31:55 +0800

    devops

--- shell/devops/create.sh
+++ shell/devops/create.sh
@@ -16,7 +16,7 @@ cat << EOF >git.sh
 # author-Jang
 # 20190423
 
-gitStatus='\${1}'
+gitStatus='\$1'
 path='\$2'
 address='\$3'
 rootPath=/alidata/www
@@ -24,7 +24,7 @@ if [ ! -d "\$rootPath"/"\$path" ];then
    	mkdir -p "\$rootPath"/"\$path";
 fi;
 if [ "\$gitStatus" == 1 ];then
-   cd "\$rootPath"/website && git clone "\$address" ./;
+   cd "\$rootPath"/"\$path"/website && git clone "\$address" ./;
 elif [ "\$gitStatus" == 2 ];then
    cd "\$rootPath"/"\$path"/website && git pull "\$address";
 else
