1b82d68570cb354336c5cd001cb53e855c1595b3
张伟江 <982215226@qq.com>
2020-04-14 09:06:12 +0800

    lnmp-nginx-1.12.2.sh修改

--- shell/lnmp-nginx-1.12.2.sh
+++ shell/lnmp-nginx-1.12.2.sh
@@ -19,6 +19,8 @@ then
     useradd -g $group -s /sbin/nolgin $user
 fi
 
+yum -y install pcre pcre-devel openssl openssl-devel
+
 rm -rf nginx-1.12.2
 if [ ! -f nginx-1.12.2.tar.gz ];then
   #wget http://nginx.org/download/nginx-1.12.2.tar.gz
@@ -26,6 +28,7 @@ if [ ! -f nginx-1.12.2.tar.gz ];then
 fi
 tar zxvf nginx-1.12.2.tar.gz
 cd nginx-1.12.2
+chmod a+x configure
 ./configure --user=www \
 --group=www \
 --prefix=/alidata/server/nginx \
@@ -42,11 +45,16 @@ fi
 make install
 chmod 775 /alidata/server/nginx/logs
 chown -R www:www /alidata/server/nginx/logs
+
+if [ ! -d "/alidata/www" ]; then
+  mkdir -p /alidata/www
+fi
+
 chmod -R 775 /alidata/www
 chown -R www:www /alidata/www
 cd ..
-cp -fR ./nginx/config-nginx/* /alidata/server/nginx/conf/
-cp -fR ./nginx/conf/nginx.conf /alidata/server/nginx/conf/
+cp -fR ./nginx-1.12.2/config-nginx/* /alidata/server/nginx/conf/
+cp -fR ./nginx-1.12.2/conf/nginx.conf /alidata/server/nginx/conf/
 sed -i 's/worker_processes  2/worker_processes  '"$CPU_NUM"'/' /alidata/server/nginx/conf/nginx.conf
 chmod 755 /alidata/server/nginx/sbin/nginx
 #/alidata/server/nginx/sbin/nginx
